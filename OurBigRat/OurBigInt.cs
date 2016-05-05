441858b74872e2acede9284dda9e37cf99dfd8f8
Samir Iskenderov <Samir_Iskenderov@epam.com>
2016-05-05 09:20:23 +0300

    Improved away.

---
+++ OurBigRat/OurBigInt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurBigRat
+{
+    public class OurBigInt
+    {
+        private uint value;
+
+        private OurBigInt previousBlock;
+
+        public OurBigInt()
+        {
+        }
+    }
+}
