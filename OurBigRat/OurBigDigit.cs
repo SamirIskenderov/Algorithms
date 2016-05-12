82446389bb39a87ffdd4453bae30ee235f5d7c5b
Samir Iskenderov <Samir_Iskenderov@epam.com>
2016-05-12 10:42:05 +0300

    Compare for bigdigit added.

--- OurBigRat/OurBigDigit.cs
+++ OurBigRat/OurBigDigit.cs
@@ -1,5 +1,6 @@
 ﻿namespace OurBigRat
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using bigint = OurBigInt;
@@ -7,8 +8,7 @@
     internal class OurBigDigit
 	{
         internal const int RADIX = 32;
-
-
+        
         internal OurBigDigit()
 		{
 			this.Value = new bool[OurBigDigit.RADIX];
@@ -20,5 +20,28 @@
 		}
 
 		internal bool[] Value { get; set; }
-	}
+
+        internal int CompareTo(OurBigDigit input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            for (int i = RADIX - 1; i >= 0; i--)
+            {
+                if ((this.Value[i] ^ input.Value[i]))
+                {
+                    if (this.Value[i])
+                    {
+                        return 1;
+                    }
+
+                    return -1;
+                }
+            }
+
+            return 0;
+        }
+    }
 }
