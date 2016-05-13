e3d51ffedff6d693ef2c1aaf67210e1a78cb8038
Samir Iskenderov <Samir_Iskenderov@epam.com>
2016-05-13 18:59:35 +0300

    CompareTo added. GetBlocksCount added. NOT TESTED.

--- BigRat/BigIntMathHelper.cs
+++ BigRat/BigIntMathHelper.cs
@@ -2,5 +2,19 @@
 {
 	internal class BigIntMathHelper : Singleton<BigIntMathHelper>
 	{
-	}
+        internal int GetBlocksCount(BigInt input)
+        {
+            int result = 0;
+            BigInt current = input;
+
+            while ((object)current != null)
+            {
+                result++;
+
+                current = current.previousBlock;
+            }
+
+            return result;
+        }
+    }
 }
