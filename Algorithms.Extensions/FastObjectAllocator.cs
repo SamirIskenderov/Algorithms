0fc9e9fb67db912463230c4e14dcc0cd27c3a698
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-06-23 18:01:57 +0300

    Removed old trash. Added cool class FastObjectAllocator<T>

---
+++ Algorithms.Extensions/FastObjectAllocator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Algorithms.Extensions
+{
+    public class FastObjectAllocator<T>
+        where T : class, new()
+    {
+        private static readonly object LockObj = new object();
+
+        private static Func<T> objCreator;
+
+        public static T New()
+        {
+            if (objCreator != null)
+            {
+                return objCreator.Invoke();
+            }
+
+            lock (LockObj)
+            {
+                if (objCreator != null)
+                {
+                    return objCreator.Invoke();
+                }
+
+                Type objType = typeof(T);
+
+                DynamicMethod meth = new DynamicMethod(name: Guid.NewGuid().ToString(),
+                                                        returnType: objType,
+                                                        parameterTypes: null);
+
+                ConstructorInfo defaultCtor = objType.GetConstructor(new Type[] { });
+
+                ILGenerator ilGen = meth.GetILGenerator();
+
+                ilGen.Emit(OpCodes.Newobj, defaultCtor);
+                ilGen.Emit(OpCodes.Ret);
+
+                objCreator = meth.CreateDelegate(typeof(Func<T>)) as Func<T>;
+            }
+
+            if (objCreator != null)
+            {
+                return objCreator.Invoke();
+            }
+
+            throw new InvalidOperationException("Can't allocate object");
+        }
+    }
+}
