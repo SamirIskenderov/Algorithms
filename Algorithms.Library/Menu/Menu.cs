00b0163ac9dd020cc1c299e9e5c1ea7dcd142b09
snowonmars <stuff4work@yandex.ru>
2016-06-29 19:35:58 +0300

    Added menu class

---
+++ Algorithms.Library/Menu/Menu.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Library.Menu
+{
+    public class Menu : ICloneable
+    {
+        private Graph graph;
+
+        public Menu()
+        {
+            this.graph = new Graph();
+            this.graph.State = State.Default;
+        }
+
+        public Menu(IEnumerable<GraphNode> nodes)
+        {
+            this.graph = new Graph(nodes);
+            this.graph.State = State.Default;
+        }
+
+        internal protected Menu(Graph graph)
+        {
+            this.graph = graph;
+            this.graph.State = State.Default;
+        }
+
+        public void AddNode()
+        {
+            this.graph.AddNode();
+        }
+
+        public void AddNode(MenuNode node)
+        {
+            this.graph.AddNode(node);
+        }
+
+        public bool IsRouteBetween(MenuNode startNode, MenuNode endNode)
+        {
+            return this.graph.IsRouteBetween(startNode, endNode);
+        }
+
+        public void RemoveNode(MenuNode node)
+        {
+            this.graph.RemoveNode(node);
+        }
+
+        #region Clone
+
+        public object Clone()
+        {
+            return (object)this.CloneDirectly();
+        }
+
+        /// <summary>
+        /// Overload of this.Clone() by return value
+        /// </summary>
+        /// <returns></returns>
+        public Menu CloneDirectly()
+        {
+            return new Menu(this.graph.Nodes);
+        }
+
+        public Menu DeepClone()
+        {
+            Graph graph = this.graph.DeepClone();
+
+            return new Menu(graph);
+        }
+
+        #endregion Clone
+    }
+}
---
+++ Algorithms.Library/Menu/MenuNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Library.Menu
+{
+    public class MenuNode : GraphNode, ICloneable
+    {
+        public string Text { get; set; }
+
+        public MenuNode(string text, int id, IEnumerable<GraphNode> connections = null, Color color = Color.White): base(id, connections, color)
+        {
+            this.Text = text;
+        }
+    }
+}
