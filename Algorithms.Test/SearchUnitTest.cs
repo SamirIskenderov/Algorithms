b2839a4590fcf28f6a1cc35a59854abd7c7a8e25
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-06-29 17:48:14 +0300

    Added sort unit test

---
+++ Algorithms.Test/SearchUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Algorithms.Extensions;
+
+namespace Algorithms.Test
+{
+    [TestClass]
+    public class SearchUnitTest
+    {
+        [TestMethod]
+        public void OverflowOfIndexsersTest()
+        {
+            const int count = 1000;
+            IList<int> array = new List<int>(count);
+            array.SetWithRandomElements(min: -10,
+                    max: 10,
+                    capacity: count,
+                    FuncToGetNewRandomElement: Extensions.IListExtensions.CommonRandom.Next);
+        }
+    }
+}
